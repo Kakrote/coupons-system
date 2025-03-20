52cda5dd1b488ce6fd02d79b58fd09e0a1ddcd21
Kakrote <anshul.pundir111@gmail.com1>
2025-03-21 02:37:12 +0530

    d

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -4,6 +4,8 @@ const claimLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 5, // 15 minutes me max 5 requests allow hongi
   message: "Too many requests! Please try again later.",
+  standardHeaders: true, // Return rate limit info in headers
+  legacyHeaders: false 
 });
 
 module.exports = claimLimiter;
