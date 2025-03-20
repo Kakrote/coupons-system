9f5269c0670f1b86a792e9ed7717dbb18d65bc73
Kakrote <anshul.pundir111@gmail.com1>
2025-03-21 02:09:11 +0530

    deployed

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://coupons-system-3seu.onrender.com/api",
   withCredentials: true,  
 });
 
